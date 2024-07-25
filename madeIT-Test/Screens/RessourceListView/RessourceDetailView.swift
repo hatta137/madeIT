5be4f2937aedcf63ae6db483e5ebf44c17043a0d
hendrik_wla <hendrik.lendeckel@fh-erfurt.de>
2024-07-25 12:51:35 +0200

    feat: design RessourceDetailView

---
+++ madeIT-Test/Screens/RessourceListView/RessourceDetailView.swift
@@ -0,0 +1,61 @@
+//
+//  RessourceDetailView.swift
+//  madeIT-Test
+//
+//  Created by Hendrik Lendeckel on 18.06.24.
+
+
+import SwiftUI
+
+struct RessourceDetailView: View {
+    
+    @State var ressource: Ressource
+    
+    var body: some View {
+        VStack {
+                       
+            
+            VStack(alignment: .leading, spacing: 5) {
+                
+                ContactInfo(image: "server.rack",           info: ressource.name)
+                ContactInfo(image: "list.bullet.clipboard", info: ressource.notes)
+                ContactInfo(image: "network",               info: ressource.ip)
+                ContactInfo(image: "network",               info: ressource.url)
+                ContactInfo(image: "person",                info: ressource.userName)
+                ContactInfo(image: "key",                   info: ressource.password)
+                
+                
+            }
+        }
+        .navigationTitle(ressource.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct ReesourceInfo: View {
+    
+    let image: String
+    let info: String
+    
+    
+    var body: some View {
+        HStack(spacing: 15) {
+            Image(systemName: image)
+                .padding(.horizontal, 10)
+            Text(info)
+        }
+        .padding(.bottom, 20)
+        
+    }
+}
+
+
+#Preview {
+    RessourceDetailView(ressource: Ressource(
+        name: "HyperV",
+        notes: "Ich stehe im Keller",
+        ip: "10.10.10.1",
+        url: "hyperv.local",
+        userName: "admin",
+        password: "admin1234"))
+}
